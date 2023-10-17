540577be19eee8dd6378c5d0655957b4b6539f87
Jake Pitman <jakeseanp@gmail.com>
2023-10-17 13:26:21 +1300

    Spin granny

--- app/Granny.tsx
+++ app/Granny.tsx
@@ -1,14 +1,26 @@
-import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF, Float } from "@react-three/drei";
 import { MeshNormalMaterial } from "three";
 
 export const Granny = () => {
   const { nodes: granny } = useGLTF("/granny.glb");
+  const grannyRef = useRef(null);
+
+  useFrame((_state, delta) => {
+    if (grannyRef.current) {
+      grannyRef.current.rotation.y += delta * 0.4;
+    }
+  });
 
   return (
-    <mesh
-      position={[0, 5, 0.5]}
-      geometry={granny.Face.geometry}
-      material={new MeshNormalMaterial()}
-    />
+    <Float>
+      <mesh
+        ref={grannyRef}
+        position={[0, 5, 0.5]}
+        geometry={granny.Face.geometry}
+        material={new MeshNormalMaterial()}
+      />
+    </Float>
   );
 };
